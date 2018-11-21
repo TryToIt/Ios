5c4631352faec748f5803fb2f5ec34522ff48a12
Panda <vesp0@yandex.ru>
2018-11-21 22:15:20 +0300

    create first hw swift project

---
+++ l1_Gusev_Anton/l1_Gusev_Anton/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  l1_Gusev_Anton
+//
+//  Created by Антон Гусев on 21.11.2018.
+//  Copyright © 2018 Антон Гусев. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
