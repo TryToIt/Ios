403e0d6dcb038ccffc4c1cc91b003394bda32a70
Panda <vesp0@yandex.ru>
2018-11-25 18:40:38 +0300

    1st task

---
+++ L1_Gusev_Anton/L1_Gusev_Anton/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  L1_Gusev_Anton
+//
+//  Created by Антон Гусев on 22.11.2018.
+//  Copyright © 2018 Антон Гусев. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
